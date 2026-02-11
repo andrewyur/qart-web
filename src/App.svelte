e3acea335ee8c27b412b2a35be05479da330b20a
Andrew Yurovchak <accounts@baetylboy.biz>
2026-02-11 14:35:32 -0500

    fix download issues on iphone, change font

--- src/App.svelte
+++ src/App.svelte
@@ -28,6 +28,10 @@
   let random = $state(false)
   let working = $state(false)
 
+  let initialLoad = true;
+  let wasmReady = wasmInit()
+
+
   function updateImage() {
     ctx1.clearRect(0,0,sideLen,sideLen)
     ctx2.clearRect(0,0,sideLen,sideLen)
@@ -38,7 +42,9 @@
     }
   }
 
-  function updatePreview() {
+  async function updatePreview() {
+    await wasmReady
+
     ctx1.drawImage(image, 0, 0, sideLen, sideLen)
 
     let imgdata = ctx1.getImageData(0,0,sideLen,sideLen)
@@ -48,6 +54,11 @@
     let preview = new Uint8ClampedArray(prepare(version, resized, brightnessThreshold, random));
 
     ctx2.putImageData(new ImageData(preview, sideLen, sideLen), 0, 0)
+
+    if(initialLoad) {
+      initialLoad = false;
+      generateCode()
+    }
   }
 
   async function generateCode() {
@@ -75,25 +86,36 @@
   }
 
   function saveImage() {
-    const anchor = document.createElement('a');
-    anchor.href = canvas3.toDataURL()
-    anchor.download = `qart-${files?.[0].name.split(".")[0]}`;
-    anchor.click();
-    anchor.remove();
+    canvas3.toBlob((blob) => {
+      if(!blob) {
+        console.error("Could not create blob from canvas contents")
+        return
+      }
+
+      const url = URL.createObjectURL(blob);
+      const anchor = document.createElement('a');
+      anchor.href = url;
+      anchor.download = `qart-${files?.[0].name.split(".")[0]}.png`;
+      anchor.click();
+      
+      // Clean up
+      setTimeout(() => {
+        URL.revokeObjectURL(url);
+        anchor.remove();
+      }, 100);
+    }, 'image/png');
   }
 
   onMount(async () => {
-    await wasmInit()
+    await wasmReady
     init();
-    updatePreview()
-    generateCode()
   })
 
 </script>
 
 <main class=" grid lg:grid-cols-[auto_auto] w-fit gap-2 lg:gap-5 grid-rows-[auto_auto] max-h-screen">
   <div class="flex flex-col gap-5 justify-center items-center">
-    <div class="flex lg:flex-row flex-col gap-2 lg:gap-5 justify-center">
+    <div class="flex lg:flex-row flex-col gap-2 lg:gap-5 justify-center items-center">
       <label for="target-url" hidden>Target URL</label>
       <input class="input" id="target-url" type="url" bind:value={url} placeholder="https://example.com"/>
       <label for="upload" hidden>Upload Image</label>
@@ -101,13 +123,10 @@
     </div>
     <div class="flex flex-row gap-5 justify-center">
       <!-- svelte-ignore a11y_missing_attribute -->
-      <img class="lg:w-45 lg:h-45 w-25 h-25 bg-stone-300" src="IMG_1145-min.JPEG" bind:this={image} onload={updatePreview}>
-    
-      <canvas class="lg:w-45 lg:h-45 w-25 h-25 bg-stone-300" bind:this={canvas1} width={sideLen} height={sideLen}></canvas>
-  
-      <canvas class="lg:w-45 lg:h-45 w-25 h-25 bg-stone-300" bind:this={canvas2} width={sideLen} height={sideLen}></canvas>
+      <img class="lg:w-45 lg:h-45 w-20 h-20 bg-stone-300" src="IMG_1145-min.JPEG" bind:this={image} onload={updatePreview}>
+      <canvas class="lg:w-45 lg:h-45 w-20 h-20 bg-stone-300" bind:this={canvas1} width={sideLen} height={sideLen}></canvas>
+      <canvas class="lg:w-45 lg:h-45 w-20 h-20 bg-stone-300" bind:this={canvas2} width={sideLen} height={sideLen}></canvas>
     </div>
-  
   
     <div class="flex flex-col gap-2 lg:gap-5 justify-center w-min">
       <label class="label">
@@ -133,9 +152,9 @@
     </div>
   </div>
 
-  <div class="flex flex-col gap-5 justify-center items-center">
+  <div class="flex flex-row lg:flex-col gap-5 justify-center items-center">
     <canvas class="h-40 lg:h-100 bg-stone-500" bind:this={canvas3} width={finalSideLen} height={finalSideLen}></canvas>
-    <div class="flex flex-row gap-5 justify-center">
+    <div class="flex flex-col lg:flex-row gap-5 justify-center">
       {#if working}
         <p class="label">working...</p>
       {:else}
@@ -146,7 +165,7 @@
   </div>
 </main>
 
-<footer class="lg:pt-0 pt-5 lg:absolute lg:bottom-5 lg:left-0 w-full text-center">
+<footer class="lg:pt-0 pt-5">
   <p class="text-sm lg:text-base">Created by <a href="https://baetylboy.biz">Andrew Yurovchak</a>. Technique by <a href="https://research.swtch.com/qart">Russ Cox</a></p>
 </footer>
 
